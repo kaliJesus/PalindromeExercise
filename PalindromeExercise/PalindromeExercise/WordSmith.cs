2cb5f0564082cc8676335e820c269400f833b0fd
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-03-01 23:33:25 -0500

    palendrome commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public static bool IsAPalindrome(string word)
+        {
+            string reversedWord = "";
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+            if (word == reversedWord)
+            {
+                return true;
+
+            }
+            else { return false; }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExerciseTests;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("mom", true)]
+        [InlineData("wow", true)]
+        [InlineData("kal", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            var wordsmith = new WordSmith();
 
+            var actual = WordSmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
